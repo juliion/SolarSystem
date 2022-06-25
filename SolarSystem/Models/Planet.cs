b652eb093f59b7c7d18eec58377bbf9f37e7e0c6
juliion <juliaionova111@gmail.com>
2022-06-25 20:24:24 +0300

    Add Models(Planet, Satellite, Star).

---
+++ SolarSystem/Models/Planet.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SolarSystem.Models
+{
+    public class Planet
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        [Required]
+        public float DistanceFromSun { get; set; }
+        [Required]
+        public float Diameter { get; set; }
+        [Required]
+        public float YearLength { get; set; }
+        [Required]
+        public float DayLength { get; set; }
+        public string PhotoPath { get; set; }
+        public string DocPath { get; set; }
+        public int StarId { get; set; }
+        [ForeignKey("StarId")]
+        public virtual Star Star { get; set; }
+        public virtual List<Satellite> Satellites { get; set; }
+
+    }
+}
---
+++ SolarSystem/Models/Star.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SolarSystem.Models
+{
+    public class Star
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        [Required]
+        public float Age { get; set; }
+        [Required]
+        public float Diameter { get; set; }
+        [Required]
+        public float Mass { get; set; }
+        [Required]
+        public float SurfaceTemp { get; set; }
+        public string PhotoPath { get; set; }
+        public string DocPath { get; set; }
+
+        public virtual List<Planet> Planets { get; set; }
+
+    }
+}
